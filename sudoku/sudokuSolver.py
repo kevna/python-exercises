4af6f24545543bcdf6862afa8aa6057563a7aacf
Aaron Moore <lakevna@gmail.com>
2016-08-08 23:58:39 +0100

    added testing for invalid solutions, only print grid on exit

--- sudoku/sudokuSolver.py
+++ sudoku/sudokuSolver.py
@@ -12,18 +12,40 @@ class SudokuSolver(object):
         self.failLimit = failLimit
         self.failedSteps = 0
 
+    def checkSolution(self):
+        for r in range(len(self.grid)):
+            for c in range(len(self.grid[r])):
+                value = self.grid[r][c].value
+                if value == None:
+                    continue
+                for R in range(len(self.grid)):
+                    if R == r:
+                        continue
+                    if self.grid[R][c].value == value:
+                        print r, c, "matches", R, c
+                        return False
+                for C in range(len(self.grid[r])):
+                    if C == c:
+                        continue
+                    if self.grid[r][C].value == value:
+                        print r, c, "matches", r, C
+                        return False
+        return True
+
     def solveStep(self):
         return False
 
     def solve(self):
         while not self.grid.isComplete():
             if self.solveStep():
-                print self.grid
+                #print self.grid
                 self.failedSteps = 0
             else:
                 self.failedSteps += 1
                 if self.failedSteps >= self.failLimit:
                     raise SolveFailedException, "Couldn't find any more moves toward solution."
+            if not self.checkSolution():
+                raise SolveFailedException, "Given Solution Invalid."
 
     @staticmethod
     def args():
@@ -38,7 +60,11 @@ class SudokuSolver(object):
         solver = self(args.filename, args.limit)
         try:
             solver.solve()
+            if not solver.checkSolution():
+                raise SolveFailedException, "Given Solution Invalid."
+            print solver.grid
         except KeyboardInterrupt:
             print "User exit."
         except SolveFailedException as err:
+            print solver.grid
             print err.message
