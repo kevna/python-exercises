851060d542d4809315332d6838d63a198b2b8e6c
kevna <lakevna@gmail.com>
2021-04-15 00:35:50 +0100

    migrate ackermann tests to pytest

--- tests/ackermann/test_ackermanCache.py
+++ tests/ackermann/test_ackermanCache.py
@@ -1,29 +1,26 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
+
 from ackermann.ackermannCache import AckermannCache
 
-class Test_Ackerman(unittest.TestCase):
-    def test_getAckermann(self):
-        testList = (
-            (-1, -1, 0),
-            (0, 0, 1),
-            (0, 5, 6),
-            (0, 10, 11),
-            (1, 0, 2),
-            (1, 5, 7),
-            (1, 10, 12),
-            (2, 0, 3),
-            (2, 5, 13),
-            (3, 0, 5),
-            (3, 5, 253),
-            (4, 0, 13),
-            (4, 1, 65533),
-                )
+class TestAckerman:
+    @pytest.mark.parametrize('testArgM, testArgN, expectedResult', (
+        (-1, -1, 0),
+        (0, 0, 1),
+        (0, 5, 6),
+        (0, 10, 11),
+        (1, 0, 2),
+        (1, 5, 7),
+        (1, 10, 12),
+        (2, 0, 3),
+        (2, 5, 13),
+        (3, 0, 5),
+        (3, 5, 253),
+        (4, 0, 13),
+        (4, 1, 65533),
+    ))
+    def test_getAckermann(self, testArgM, testArgN, expectedResult):
         testCache = AckermannCache()
-        for testArgM, testArgN, expectedResult in testList:
-            actualResult = testCache.getAckermann(testArgM, testArgN)
-            self.assertEqual(expectedResult, actualResult)
-
-if __name__ == "__main__":
-    unittest.main()
+        actualResult = testCache.getAckermann(testArgM, testArgN)
+        assert actualResult == expectedResult
