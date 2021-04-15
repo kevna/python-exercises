2796df7ec231a8523958bee38d0b4c8677f6e476
kevna <lakevna@gmail.com>
2021-04-15 23:53:31 +0100

    migrate game of life tests to pytest

--- tests/life/test_gameOfLife.py
+++ tests/life/test_gameOfLife.py
@@ -1,118 +1,111 @@
 #!/usr/bin/python2
 # -*- coding: utf-8 -*-
-import unittest
+import pytest
+
 from life.gameOfLife import GameOfLife
 
-class Test_GameOfLife(unittest.TestCase):
-    def test_cellLives(self):
-        testList = (
-            ([
-                [False, True , False],
-                [False, True , False],
-                [False, True , False],
-            ], 1, 0, True),
-            ([
-                [False, False, False],
-                [True , True , True ],
-                [False, False, False],
-            ], 0, 1, True),
-            ([
-                [False, False, False, False],
-                [False, True , True , False],
-                [False, True , True , False],
-                [False, False, False, False],
-            ], 1, 0, False),
-            ([
-                [False, False, False, False],
-                [False, True , True , False],
-                [False, True , True , False],
-                [False, False, False, False],
-            ], 1, 1, True),
-            ([
-                [False, False, False],
-                [True , True , True ],
-                [False, False, False],
-            ], 0, -1, False),
-            #([], 0, 0, False),
-                )
-        for startGrid, testArgX, testArgY, expectedResult in testList:
-            testGrid = GameOfLife(startGrid)
-            actualResult = testGrid.cellLives(testArgX, testArgY)
-            self.assertEqual(expectedResult, actualResult)
+class TestGameOfLife:
+    @pytest.mark.parametrize('startGrid, testArgX, testArgY, expectedResult', (
+        ([
+            [False, True , False],
+            [False, True , False],
+            [False, True , False],
+        ], 1, 0, True),
+        ([
+            [False, False, False],
+            [True , True , True ],
+            [False, False, False],
+        ], 0, 1, True),
+        ([
+            [False, False, False, False],
+            [False, True , True , False],
+            [False, True , True , False],
+            [False, False, False, False],
+        ], 1, 0, False),
+        ([
+            [False, False, False, False],
+            [False, True , True , False],
+            [False, True , True , False],
+            [False, False, False, False],
+        ], 1, 1, True),
+        ([
+            [False, False, False],
+            [True , True , True ],
+            [False, False, False],
+        ], 0, -1, False),
+        #([], 0, 0, False),
+    ))
+    def test_cellLives(self, startGrid, testArgX, testArgY, expectedResult):
+        testGrid = GameOfLife(startGrid)
+        actualResult = testGrid.cellLives(testArgX, testArgY)
+        assert actualResult == expectedResult
     
-    def test_step(self):
-        testList = (
-            ([
-                [False, True , False],
-                [False, True , False],
-                [False, True , False],
-            ],
-            [
-                [False, False, False],
-                [True , True , True ],
-                [False, False, False],
-            ]),
-            ([
-                [False, False, False, False],
-                [False, True , True , False],
-                [False, True , True , False],
-                [False, False, False, False],
-            ],
-            [
-                [False, False, False, False],
-                [False, True , True , False],
-                [False, True , True , False],
-                [False, False, False, False],
-            ]),
-                )
-        for startGrid, expectedResult in testList:
-            testGrid = GameOfLife(startGrid)
-            testGrid.step()
-            actualResult = testGrid.grid
-            self.assertEqual(expectedResult, actualResult)
+    @pytest.mark.parametrize('startGrid, expectedResult', (
+        ([
+            [False, True , False],
+            [False, True , False],
+            [False, True , False],
+        ],
+        [
+            [False, False, False],
+            [True , True , True ],
+            [False, False, False],
+        ]),
+        ([
+            [False, False, False, False],
+            [False, True , True , False],
+            [False, True , True , False],
+            [False, False, False, False],
+        ],
+        [
+            [False, False, False, False],
+            [False, True , True , False],
+            [False, True , True , False],
+            [False, False, False, False],
+        ]),
+    ))
+    def test_step(self, startGrid, expectedResult):
+        testGrid = GameOfLife(startGrid)
+        testGrid.step()
+        actualResult = testGrid.grid
+        assert actualResult == expectedResult
 
-    def test_str(self):
-        testList = (
-            ([
-                [False, True , False],
-                [False, True , False],
-                [False, True , False],
-            ], " \xe2\x96\x88 \n \xe2\x96\x88 \n \xe2\x96\x88 "),
-            ([
-                [False, False, False],
-                [True , True , True ],
-                [False, False, False],
-            ], "   \n\xe2\x96\x88\xe2\x96\x88\xe2\x96\x88\n   "),
-            ([
-                [False, False, False, False],
-                [False, True , True , False],
-                [False, True , True , False],
-                [False, False, False, False],
-            ], "    \n \xe2\x96\x88\xe2\x96\x88 \n \xe2\x96\x88\xe2\x96\x88 \n    "),
-            #([], 0, 0, False),
-                )
-        for startGrid, expectedResult in testList:
-            testGrid = GameOfLife(startGrid)
-            actualResult = str(testGrid)
-            self.assertEqual(expectedResult, actualResult)
+    @pytest.mark.parametrize('startGrid, expectedResult', (
+        ([
+            [False, True , False],
+            [False, True , False],
+            [False, True , False],
+        ], " \xe2\x96\x88 \n \xe2\x96\x88 \n \xe2\x96\x88 "),
+        ([
+            [False, False, False],
+            [True , True , True ],
+            [False, False, False],
+        ], "   \n\xe2\x96\x88\xe2\x96\x88\xe2\x96\x88\n   "),
+        ([
+            [False, False, False, False],
+            [False, True , True , False],
+            [False, True , True , False],
+            [False, False, False, False],
+        ], "    \n \xe2\x96\x88\xe2\x96\x88 \n \xe2\x96\x88\xe2\x96\x88 \n    "),
+        #([], 0, 0, False),
+    ))
+    def test_str(self, startGrid, expectedResult):
+        testGrid = GameOfLife(startGrid)
+        actualResult = str(testGrid)
+        assert actualResult == expectedResult
 
-    def test_randomGrid(self):
-        testList = (
-            (0, 0, 0, 0),
-            (1, 0, 1, 0),
-            (10, 10, 10, 10),
-            (50, 100, 50, 100),
-            (-1, -10, 0, 0),
-            #([], 0, 0, False),
-                )
-        for testArgX, testArgY, expectedX, expectedY in testList:
-            testGrid = GameOfLife.randomGrid(testArgX, testArgY)
-            actualX = len(testGrid)
-            self.assertEqual(expectedX, actualX)
-            for x in testGrid:
-                actualY = len(x)
-                self.assertEqual(expectedY, actualY)
-
-
-if __name__ == "__main__":
-    unittest.main()
+    @pytest.mark.parametrize('testArgX, testArgY, expectedX, expectedY', (
+        (0, 0, 0, 0),
+        (1, 0, 1, 0),
+        (10, 10, 10, 10),
+        (50, 100, 50, 100),
+        (-1, -10, 0, 0),
+        #([], 0, 0, False),
+    ))
+    def test_randomGrid(self, testArgX, testArgY, expectedX, expectedY):
+        testGrid = GameOfLife.randomGrid(testArgX, testArgY)
+        actualX = len(testGrid)
+        assert actualX == expectedX
+        for x in testGrid:
+            actualY = len(x)
+            assert actualY == expectedY
