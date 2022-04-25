50b542d4e6ba426e5e18f9b8b49c0a90b5ca8ce7
Aaron Moore <lakevna@gmail.com>
2022-04-25 18:54:42 +0100

    refactor: missing docstrings

--- src/sorting/sort_demo.py
+++ src/sorting/sort_demo.py
@@ -1,4 +1,9 @@
+"""This module unfinished as it's inherited from a university module
+"""
+
 def read_data(filename):
+    """Read sampole data file to demonstrate sorting.
+    """
     result = []
     with open(filename, encoding='utf-8') as file:
         for line in file:
